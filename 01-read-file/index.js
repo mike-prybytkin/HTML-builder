deafaeefb6ac92d0ca00cc5d20eec57bcc40c3b2
@Mike-Prybytkin <prybytkin@bk.ru>
2022-05-19 23:34:37 +0300

    refactor: 01-read-file task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,4 +3,7 @@ const path = require('path');
 
 const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
 
-stream.on('data', (chunk) => console.log(chunk));
+let data = '';
+
+stream.on('data', (chunk) => (data += chunk));
+stream.on('end', () => console.log(data));
